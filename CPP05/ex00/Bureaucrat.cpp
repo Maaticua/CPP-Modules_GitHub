80dc29aa49f392629585bda0923bd23bfe304747
Maati Caruana <mhp.cua@gmail.com>
2026-01-22 13:30:23 +0100

    almost finised

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:27:41 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/22 11:21:20 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:53:43 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name)
 {
 	if (grade < 1)
 		throw GradeTooHighException();
+
 	if (grade > 150)
 		throw GradeTooLowException();
-		this->grade = grade;
+	this->grade = grade;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade)
--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,19 +6,95 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:27:49 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/22 11:28:32 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/22 13:29:03 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include <iostream>
 
 int main()
 {
-	Bureaucrat B1;
-	
-	Bureaucrat("B2", 1);
-	Bureaucrat("B3", 150);
-	Bureaucrat("B4", 75);
+	std::cout << "=== Test 1: Creation de bureaucrates valide ===" << std::endl;
+	try
+	{
+		Bureaucrat B1("B1", 1);
+		std::cout << B1 << std::endl;
 
+		Bureaucrat B2("B2", 75);
+		std::cout << B2 << std::endl;
+
+		Bureaucrat B3("B3", 150);
+		std::cout << B3 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception" << e.what() << '\n';
+	}
+
+	std::cout << std::endl;
+	std::cout << "=== Test 2: Creation de bureaucrates TooBig ===" << std::endl;
+	try
+	{
+		Bureaucrat IB1("TooHighBureaucrat", 0);
+		std::cout << IB1 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << '\n';
+	}
+
+	std::cout << std::endl;
+	std::cout << "=== Test 3: Creation de bureaucrates TooLow ===" << std::endl;
+	try
+	{
+		Bureaucrat IB2("TooLowBureaucrat", 151);
+		std::cout << IB2 << std::endl;
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << '\n';
+	}
+
+	std::cout << std::endl;
+	std::cout << "=== Test 4: Incrementation du grade jusqu'a l'echec ===" << std::endl;
+	try
+	{
+		Bureaucrat B4("B4", 3);
+		std::cout << "Avant: " << B4 << std::endl;
+		B4.incrementGrade();
+		std::cout << "Apres incrementation: " << B4 << std::endl;
+		B4.incrementGrade();
+		std::cout << "Apres incrementation: " << B4 << std::endl;
+		B4.incrementGrade();
+		std::cout << "Apres incrementation: " << B4 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << '\n';
+	}
+
+
+	std::cout << std::endl;
+	std::cout << "=== Test 5: Decrementation du grade jusqu'a l'echec ===" << std::endl;
+	try
+	{
+		Bureaucrat B5("B5", 148);
+		std::cout << "Avant: " << B5 << std::endl;
+		B5.decrementGrade();
+		std::cout << "Apres decrementation: " << B5 << std::endl;
+		B5.decrementGrade();
+		std::cout << "Apres decrementation: " << B5 << std::endl;
+		B5.decrementGrade();
+		std::cout << "Apres decrementation: " << B5 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << '\n';
+	}
+
+
+	
 	return 0;
 }
