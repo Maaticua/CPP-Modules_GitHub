ae0291d470701ceeaed2209f73928803ca4713c4
Maati Caruana <mhp.cua@gmail.com>
2026-01-23 15:52:30 +0100

    maj

--- CPP05/ex02/PresidentialPardonForm.cpp
+++ CPP05/ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,39 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:33:12 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/23 10:33:13 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/23 15:49:57 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardonForm", 25, 5), target("default")
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) : AForm("ShrubberyCreation", 25, 5), target(target)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other) : AForm(other), target(other.target)
+{
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		this->target = other.target;
+	}
+	return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const & execute) const
+{
+
+}
