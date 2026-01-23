2f6467b09f1f53dad808b719fbd4c0957001d0a1
Maati Caruana <mhp.cua@gmail.com>
2026-01-23 15:45:29 +0100

    maj

--- CPP05/ex02/RobotomyRequestForm.cpp
+++ CPP05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,50 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:33:17 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/23 10:33:18 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/23 15:45:12 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45), target("default")
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target) : AForm("ShrubberyCreation", 72, 45), target(target)
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other) : AForm(other), target(other.target)
+{
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		this->target = other.target;
+	}
+	return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const & executor) const
+{
+	if (!this->getSigned())
+		throw FormNotSignedException();
+	if (executor.getGrade() > this->getGradeToExecute())
+		throw GradeTooLowException();
+
+	std::cout << "* BZZZZZZZZZZZ * Drilling noises * BZZZZZZZ *" << std::endl;
+
+	if (std::rand() % 2 == 0)
+		std::cout << this->target << " has been robotomized successfully !" << std::endl;
+	else
+		std::cout << "Robotomy failed on " << this->target << " !" << std::endl;
+
+}
--- CPP05/ex02/RobotomyRequestForm.hpp
+++ CPP05/ex02/RobotomyRequestForm.hpp
@@ -6,7 +6,29 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:33:19 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/23 10:33:20 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/23 15:43:51 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+#include <cstdlib>
+#include <ctime>
+
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string target;
+	public :
+		RobotomyRequestForm();
+		RobotomyRequestForm(const std::string& target);
+		RobotomyRequestForm(const RobotomyRequestForm& other);
+		RobotomyRequestForm& operator=(const RobotomyRequestForm& other);
+		~RobotomyRequestForm();
+
+		virtual void execute(Bureaucrat const & executor) const;
+};
+
+#endif
