fab86f08d9977c7d32543c8a67618b6efbf4ba82
Maati Caruana <mhp.cua@gmail.com>
2026-01-26 12:17:52 +0100

    Intern.hpp/cpp finished

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -6,8 +6,62 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 11:39:12 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/26 11:39:45 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/26 12:17:25 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Intern.hpp"
+
+
+Intern::Intern()
+{
+}
+
+Intern::Intern(const Intern& other)
+{
+	(void)other;
+}
+
+Intern& Intern::operator=(const Intern& other)
+{
+	(void)other;
+	return *this;
+}
+
+Intern::~Intern()
+{
+}
+
+static AForm* createShrubbery(std::string target)
+{
+	return new ShrubberyCreationForm(target);
+}
+
+static AForm* createRobotomy(std::string target)
+{
+	return new RobotomyRequestForm(target);
+}
+
+static AForm* createPresidential(std::string target)
+{
+	return new PresidentialPardonForm(target);
+}
+AForm* Intern::makeForm(std::string forName, std::string target)
+{
+	std::string formType[3] = {"ShrubberyCreation" , "RobotomyRequest", "PresidentialPardon"}; // * Tab des noms des forms
+	AForm* (*formCreators[3])(std::string) = {&createShrubbery, &createRobotomy, &createPresidential}; // * Tab de * sur fonction
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (forName == formType[i])
+		{
+			std::cout << "Intern creates " << forName << std::endl;
+			return formCreators[i](target);
+		}
+	}
+
+	std::cerr << "Error: Form type \"" << forName << "\" does not exist !" << std::endl;
+	return NULL;
+
+}
--- CPP05/ex03/Intern.hpp
+++ CPP05/ex03/Intern.hpp
@@ -6,7 +6,29 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 11:39:18 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/26 11:39:43 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/26 11:49:04 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+#include <string>
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+class Intern
+{
+	public:
+		Intern();
+		Intern(const Intern& other);
+		Intern& operator=(const Intern& other);
+		~Intern();
+
+		AForm* makeForm(std::string forName, std::string target);
+};
+
+#endif
