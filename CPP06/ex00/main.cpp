e82ade4f5b997c41ddbca9bfbc25a16e954e8117
Maati Caruana <mhp.cua@gmail.com>
2026-01-28 12:19:52 +0100

    00 finished

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,11 +6,20 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 11:33:53 by macaruan          #+#    #+#             */
-/*   Updated: 2026/01/28 11:34:02 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/28 12:16:06 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
 int main(int ac, char **av)
 {
-	
+	if (ac != 2)
+	{
+		std::cout << "Bad arg: ./convert <>" << std::endl;
+		return 1;
+	}
+
+	ScalarConverter::convert(av[1]);
+	return 0;
 }
