681c77d9a615ff689450e61dbae8bf8b81ccf94a
Maati Caruana <mhp.cua@gmail.com>
2026-02-05 15:07:15 +0100

    fixing minor bugs

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 16:28:01 by macaruan          #+#    #+#             */
-/*   Updated: 2026/02/05 14:43:31 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/02/05 15:06:34 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,26 @@ int main()
 		sp.addNumber(17);
 		sp.addNumber(9);
 		sp.addNumber(11);
-		std::cout << "Shortest : " <<  YELLOW << sp.shortestSpan() << RESET << std::endl;
-		std::cout << "Longest : " <<sp.longestSpan() << std::endl;
+		std::cout << YELLOW << "Shortest : " << RESET << sp.shortestSpan() << std::endl;
+		std::cout << YELLOW << "Longest : " << RESET << sp.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	std::cout << GREEN << "=== Test 2 : Additional test ===" << RESET << std::endl;
+	try
+	{
+		Span sp = Span(17);
+		sp.addNumber(43);
+		sp.addNumber(5);
+		sp.addNumber(20);
+		sp.addNumber(1);
+		sp.addNumber(116);
+		std::cout << YELLOW << "Shortest : " << RESET << sp.shortestSpan() << std::endl;
+		std::cout << YELLOW << "Longest : " << RESET << sp.longestSpan() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
