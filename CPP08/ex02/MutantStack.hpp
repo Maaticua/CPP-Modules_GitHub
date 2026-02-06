ecc55ff0c72c01a6f6956830b9459b9e870adcf8
Maati Caruana <mhp.cua@gmail.com>
2026-02-06 13:43:34 +0100

    fixed issues with yassou

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,15 +6,39 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 15:24:03 by macaruan          #+#    #+#             */
-/*   Updated: 2026/02/05 15:27:40 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/02/06 13:14:20 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-class MutantStack
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+template <typename T> class MutantStack : public std::stack<T>
 {
+	private:
+
+	public :
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		MutantStack() : std::stack<T>() {};
+		MutantStack(const MutantStack& other) : std::stack<T>(other) {};
+		MutantStack& operator=(const MutantStack& other) {if (this != &other) std::stack<T>::operator=(other); return *this;};
+		~MutantStack() {};
+
+		iterator begin() { return this->c.begin(); };
+		iterator end() { return this->c.end(); };
+		const_iterator begin() const { return this->c.begin(); };
+		const_iterator end() const { return this->c.end(); };
+		iterator rbegin() { return this->c.rbegin(); };
+		iterator rend() { return this->c.rend(); };
 
 };
 
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,8 +6,80 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 15:23:58 by macaruan          #+#    #+#             */
-/*   Updated: 2026/02/05 15:24:54 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/02/06 13:42:41 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+#include <list>
+
+int main()
+{
+	std::cout << "=== Test 1 : mstack ===" << std::endl;
+
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << std::endl;
+	std::cout << "=== Test 2 : list ===" << std::endl;
+
+	std::list<int> mlist;
+
+	mlist.push_front(5);
+	mlist.push_front(17);
+
+	std::cout << mlist.front() << std::endl;
+
+	mlist.pop_front();
+
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+
+	std::list<int>::iterator it2 = mlist.begin();
+	std::list<int>::iterator ite2 = mlist.end();
+
+	++it2;
+	--it2;
+
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::list<int> s2(mlist);
+
+
+
+	return 0;
+}
