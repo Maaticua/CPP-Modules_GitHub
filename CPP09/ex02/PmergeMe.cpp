8e086506c79f5fecc3d9cbbe8ef0769229a2d729
Maati Caruana <mhp.cua@gmail.com>
2026-02-16 16:00:46 +0100

    starting ex02

---
+++ CPP09/ex02/PmergeMe.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 15:27:09 by macaruan          #+#    #+#             */
+/*   Updated: 2026/02/16 16:00:25 by macaruan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe &src)
+{
+	*this = src;
+}
+PmergeMe &PmergeMe::operator=(const PmergeMe &src)
+{
+	if (this != &src)
+	{
+		_vec = src._vec;
+		_deq = src._deq;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+//? --- Ford Johnson vector logique ---
---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 15:27:23 by macaruan          #+#    #+#             */
+/*   Updated: 2026/02/16 15:54:37 by macaruan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <iterator>
+#include <sys/time.h>
+
+class PmergeMe
+{
+	private:
+		std::vector<int> _vec;
+		std::deque<int> _deq;
+
+		// ? Ford Johnson version <vector>
+		void sortVector();
+		std::vector<int> fordJohnsonVector(std::vector<int>& lst);
+
+		// ! Ford Johnson version <deque>
+		void sortDeque();
+		std::deque<int> fordJohnsonDeque(std::deque<int>& lst);
+
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe &src);
+		PmergeMe &operator=(const PmergeMe &src);
+		~PmergeMe();
+
+		void run(int ac, char **av);
+};
+
+#endif
