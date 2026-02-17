26fd5be5cc3d3ce7f6d10e6f5392415f96793a93
Maati Caruana <mhp.cua@gmail.com>
2026-02-17 16:33:42 +0100

    progression du ex02

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 15:27:09 by macaruan          #+#    #+#             */
-/*   Updated: 2026/02/16 16:00:25 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/02/17 13:50:01 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,42 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &src)
 PmergeMe::~PmergeMe() {}
 
 //? --- Ford Johnson vector logique ---
+std::vector<int> PmergeMe::fordJohnsonVector(std::vector<int>& lst)
+{
+	if (lst.size() <= 1)
+		return lst;
+
+	int odd = -1;
+	if (lst.size() % 2 != 0)
+	{
+		odd = lst.back();
+		lst.pop_back();
+	}
+	std::vector<std::pair<int, int> > pairs;
+	for (size_t i = 0; i < lst.size(); i+= 2)
+	{
+		if (lst[i] < lst[i + 1])
+			std::swap(lst[i], lst[i + 1]);
+		pairs.push_back(std::make_pair(lst[i], lst[i + 1]));
+	}
+
+	std::vector<int> higher;
+	for (size_t i = 0; i < pairs.size(); i++)
+		higher.push_back(pairs[i].first);
+	higher = fordJohnsonVector(higher);
+
+	std::vector<int> main_chain = higher;
+	std::vector<int>pend;
+	for (size_t i = 0; i < pairs.size(); ++i)
+	{
+		for (size_t j = 0; j < pairs.size(); ++i)
+		{
+			if (pairs[j].first == main_chain[i])
+			{
+				pend.push_back(pairs[j].second);
+				break;
+			}
+		}
+	}
+	
+}
