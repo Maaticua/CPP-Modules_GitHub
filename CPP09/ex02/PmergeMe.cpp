9043c392ee283fb498177882c468ea872bddb096
Maati Caruana <mhp.cua@gmail.com>
2026-02-18 10:24:23 +0100

    siuu

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 15:27:09 by macaruan          #+#    #+#             */
-/*   Updated: 2026/02/17 13:50:01 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/02/18 10:23:40 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,50 @@ std::vector<int> PmergeMe::fordJohnsonVector(std::vector<int>& lst)
 			}
 		}
 	}
+	for (size_t i = 0; i < pend.size(); ++i)
+	{
+		std::vector<int>::iterator it = std::lower_bound(main_chain.begin(), main_chain.end(), pend[i]);
+		main_chain.insert(it, pend[i]);
+	}
+	if (odd != -1)
+	{
+		std::vector<int>::iterator it = std::lower_bound(main_chain.begin(), main_chain.end(), odd);
+		main_chain.insert(it, odd);
+	}
+	return main_chain;
+}
+
+void PmergeMe::run(int ac, char **av)
+{
+	for (int i = 1; i < ac; ++i)
+	{
+		int val = std::atoi(av[i]);
+		if (val < 0)
+		{
+			std::cerr << "Error" << std::endl;
+			return;
+		}
+		_vec.push_back(val);
+		_deq.push_back(val);
+	}
+
+	std::cout << "Before: ";
+	for (size_t i = 0; i < _vec.size() && i < 10; ++i)
+		std::cout << _vec[i] << " ";
+	if (_vec.size() > 10)
+		std::cout << "[...]" << std::endl;
+
+	//? Time pour vector
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	_vec = fordJohnsonVector(_vec);
+	gettimeofday(&end, NULL);
+	long long vec_time = (end.tv_sec - start.tv_sec) * 1000000LL + (end.tv_usec - start.tv_usec);
+
+	//! Time pour deque
+	gettimeofday(&start, NULL);
+	_deq = fordJohnsonDeque(_deq);
+	gettimeofday(&end, NULL);
+	long long vec_time = (end.tv_sec - start.tv_sec) * 1000000LL + (end.tv_usec - start.tv_usec);
 	
 }
